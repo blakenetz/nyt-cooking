b3875250b579d85452df617fe5c6b0eb38dc2279
netzebandito <blake.netzeband@gmail.com>
2020-12-30 13:15:25 -0700

    updated to run on window load event

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # nyt-cooking
 
 Firefox extension for [NYT cooking](https://cooking.nytimes.com/)
+
+## Installation
+
+### Temporary
+
+[Follow these instructions](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension#Installing)
+
+### Self-distributed
+
+- Create Mozilla profile/add-on key (https://addons.mozilla.org/developers/addon/api/key/)
+- Run [web-ext](https://extensionworkshop.com/documentation/develop/getting-started-with-web-ext/) build command
+
+```
+$ npx web-ext sign --api-key=$AMO_JWT_ISSUER --api-secret=$AMO_JWT_SECRET
+```
--- index.js
+++ index.js
@@ -1,9 +1,13 @@
-// remove noScroll css class
-[...document.querySelectorAll('[class*="noScroll"]')].forEach((node) => {
-	const [className] = [...node.classList].filter(
-		(cl) => cl.indexOf("noScroll") > -1
+window.onload = function () {
+	// remove noScroll css class
+	[...document.querySelectorAll('[class*="noScroll"]')].forEach((node) => {
+		const [className] = [...node.classList].filter(
+			(cl) => cl.indexOf("noScroll") > -1
+		);
+		node.classList.remove(className);
+	});
+	// remove all modals
+	[...document.querySelectorAll('[role="dialog"]')].forEach((el) =>
+		el.remove()
 	);
-	node.classList.remove(className);
-});
-// remove all modals
-[...document.querySelectorAll('[role="dialog"]')].forEach((el) => el.remove());
+};
