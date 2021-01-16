7a7e5cb979bc9a5a338d1aac01d0162c9973b0d7
netzebandito <blake.netzeband@gmail.com>
2021-01-16 08:08:37 -0700

    up version. formatting

--- index.js
+++ index.js
@@ -1,28 +1,32 @@
-const noScrollNodeList = ['html', 'body'].map(sel => document.querySelector(sel))
+const noScrollNodeList = ["html", "body"].map(sel => {
+	return document.querySelector(sel);
+});
 const config = {
 	attibutes: true,
-	attributeFilter: ['class']
-}
+	attributeFilter: ["class"],
+};
 
 function removeModals() {
-	[...document.querySelectorAll('[role="dialog"]')].forEach((el) =>
-		el.remove()
-	);
+	[...document.querySelectorAll('[role="dialog"]')].forEach(el => {
+		el.remove();
+	});
 }
 function removeNoScrollClass(node) {
-	[...node.classList].filter(
-		(cl) => cl.indexOf("noScroll") > -1
-	).forEach(cl => node.classList.remove(cl))
-
+	[...node.classList]
+		.filter(cl => cl.indexOf("noScroll") > -1)
+		.forEach(cl => node.classList.remove(cl));
 }
 
 window.onload = function () {
 	removeModals();
-	[...document.querySelectorAll('[class*="noScroll"]')].forEach((node) => removeNoScrollClass(node));
+	[...document.querySelectorAll('[class*="noScroll"]')].forEach(node => {
+		removeNoScrollClass(node);
+	});
 };
 
-const observer = new MutationObserver((mutationsList) => {
-	mutationsList.forEach(mutation => removeNoScrollClass(mutation.target))
+const observer = new MutationObserver(mutationsList => {
+	mutationsList.forEach(mutation => removeNoScrollClass(mutation.target));
+	removeModals();
 });
 
-noScrollNodeList.forEach(el => observer.observe(el, config))
+noScrollNodeList.forEach(el => observer.observe(el, config));
