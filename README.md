d1fbad88205cf840803b750b14e5a8cc780d46a5
netzebandito <blake.netzeband@gmail.com>
2021-01-17 10:14:56 -0700

    wrap in iife

--- README.md
+++ README.md
@@ -2,9 +2,9 @@
 
 Firefox extension for [NYT cooking](https://cooking.nytimes.com/)
 
-## Installation
+## Firefox
 
-### Temporary
+### Temporary installation
 
 [Follow these instructions](https://developer.mozilla.org/en-US/docs/Mozilla/Add-ons/WebExtensions/Your_first_WebExtension#Installing)
 
@@ -12,5 +12,10 @@ Firefox extension for [NYT cooking](https://cooking.nytimes.com/)
 
 - Create Mozilla profile/add-on [key](https://addons.mozilla.org/developers/addon/api/key/)
 - Update .env file
-- Run `sign` and `build` commands
+- Run `sign:android` and `build:android` commands
 - Install(https://extensionworkshop.com/documentation/publish/distribute-sideloading/)
+
+## Chrome
+
+- Unpack extensions in [extensions settings](chrome://extensions/)
+- Alternatively, follow [these instructions](https://developer.chrome.com/docs/extensions/mv2/getstarted/#manifest)
--- index.js
+++ index.js
@@ -1,32 +1,34 @@
-const noScrollNodeList = ["html", "body"].map(sel => {
-	return document.querySelector(sel);
-});
-const config = {
-	attibutes: true,
-	attributeFilter: ["class"],
-};
-
-function removeModals() {
-	[...document.querySelectorAll('[role="dialog"]')].forEach(el => {
-		el.remove();
+(function () {
+	const noScrollNodeList = ["html", "body"].map(sel => {
+		return document.querySelector(sel);
 	});
-}
-function removeNoScrollClass(node) {
-	[...node.classList]
-		.filter(cl => cl.indexOf("noScroll") > -1)
-		.forEach(cl => node.classList.remove(cl));
-}
+	const config = {
+		attibutes: true,
+		attributeFilter: ["class"],
+	};
 
-window.onload = function () {
-	removeModals();
-	[...document.querySelectorAll('[class*="noScroll"]')].forEach(node => {
-		removeNoScrollClass(node);
+	function removeModals() {
+		[...document.querySelectorAll('[role="dialog"]')].forEach(el => {
+			el.remove();
+		});
+	}
+	function removeNoScrollClass(node) {
+		[...node.classList]
+			.filter(cl => cl.indexOf("noScroll") > -1)
+			.forEach(cl => node.classList.remove(cl));
+	}
+
+	window.onload = function () {
+		removeModals();
+		[...document.querySelectorAll('[class*="noScroll"]')].forEach(node => {
+			removeNoScrollClass(node);
+		});
+	};
+
+	const observer = new MutationObserver(mutationsList => {
+		mutationsList.forEach(mutation => removeNoScrollClass(mutation.target));
+		removeModals();
 	});
-};
 
-const observer = new MutationObserver(mutationsList => {
-	mutationsList.forEach(mutation => removeNoScrollClass(mutation.target));
-	removeModals();
-});
-
-noScrollNodeList.forEach(el => observer.observe(el, config));
+	noScrollNodeList.forEach(el => observer.observe(el, config));
+})();
